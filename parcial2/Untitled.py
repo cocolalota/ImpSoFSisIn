672e0082877c514ef4185b6f407de92610e4d3ba
cocolalota <palacios.victor.2024@cbtis-246.edu.mx>
2025-03-25 18:15:45 -0600

    Add files via upload

---
+++ parcial2/Untitled.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def fizzbuzz(max_num):
+    three_mul = 'fizz'
+    five_mul = 'buzz'
+    num1 = 3
+    num2 = 5 
+
+    for i in range(1,max_num + 1):
+        if i%num1==0 and i%num2==0:
+            print(i,three_mul+five_mul)
+        elif i%num1==0:
+            print(i,three_mul)
+        elif i%num2==0:
+            print(i,five_mul)
+
+#----START OF SCRIPT
+if __name__=='__main__':
+    fizzbuzz(99)
+
+
+# In[ ]:
+
+
+
+
